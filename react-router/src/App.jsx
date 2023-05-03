f1a37bac272ae6f606ecc83574c3484ae4bfe326
UCT <uct@ets-mac-0238.local>
2023-05-03 17:51:27 +0300

    Link react router dom

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
 	return (
-		<>
+		<div>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
 			</Routes>
-		</>
+		</div>
 	);
 }
 
