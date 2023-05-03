cd0d3694314093bcdfc8b267df83ca6dc51a94fa
UCT <uct@ets-mac-0238.local>
2023-05-04 01:12:11 +0300

    contact page

--- burger-project/src/components/Contact.jsx
+++ burger-project/src/components/Contact.jsx
@@ -1,5 +1,21 @@
-const Contact = () => {
-	return <div>Contact</div>;
-};
+import BannerImage from '../assets/banner.png';
+import '../styles/contact.css';
 
-export default Contact;
+export const Contact = () => {
+	return (
+		<div className="contact">
+			<div className="leftSide" style={{ backgroundImage: `url(${BannerImage})` }}></div>
+			<div className="rightSide">
+				<h1>Bizimle İletişime Geçin</h1>
+				<form>
+					<label>Ad Soyad</label>
+					<input type="text" name="name" placeholder="Lütfen adınızı soyadınızı giriniz..." />
+					<label>Email</label>
+					<input type="email" name="email" placeholder="Lütfen emailinizi giriniz..." />
+					<label>Mesajınız</label>
+					<textarea rows="6" name="message" placeholder="Lütfen mesajınızı giriniz..."></textarea>
+				</form>
+			</div>
+		</div>
+	);
+};
