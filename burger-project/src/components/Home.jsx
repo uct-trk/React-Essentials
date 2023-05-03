c5f1b2a0527bc7e7e8e71f13e9a0207607c22f04
UCT <uct@ets-mac-0238.local>
2023-05-04 00:50:38 +0300

    homepage

--- burger-project/src/components/Home.jsx
+++ burger-project/src/components/Home.jsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+import BannerImage from '../assets/banneryeni.jpg';
+import '../styles/home.css';
+
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div className="mainPage" style={{ backgroundImage: `url(${BannerImage})` }}>
+			<div className="order">
+				<Link to="/menu">
+					<button>SİPARİŞ VER</button>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
