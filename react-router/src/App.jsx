eed370a0c7ef1c448878cebd44fa1086115741c0
UCT <uct@ets-mac-0238.local>
2023-05-03 17:44:23 +0300

    react roıuter dom basic routing

---
+++ react-router/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+
+function App() {
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+			</Routes>
+		</>
+	);
+}
+
+export default App;
