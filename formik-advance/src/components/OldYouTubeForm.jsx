1de4a514b033194b755ce9bfae6fe9b2853baa2a
UCT <uct@ets-mac-0238.local>
2023-05-10 17:36:21 +0300

    getFieldProps

--- formik-advance/src/components/OldYouTubeForm.jsx
+++ formik-advance/src/components/OldYouTubeForm.jsx
@@ -11,6 +11,23 @@ const onSubmit = (values) => {
 	console.group(values, 'form data');
 };
 
+const validate = (values) => {
+	let errors = {};
+	if (!values.name) {
+		errors.name = 'Required';
+	}
+	if (!values.email) {
+		errors.email = 'Required';
+	} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+		errors.email = 'Invalid email format';
+	}
+	if (!values.channel) {
+		errors.channel = 'Required';
+	}
+
+	return errors;
+};
+
 // yup validasyon şeması. Validsyon işlemlerini yönetiyoruz
 const validationSchema = Yup.object({
 	name: Yup.string().required('Required'),
@@ -30,17 +47,17 @@ const YouTubeForm = () => {
 			<form onSubmit={formik.handleSubmit}>
 				<div className="form-control">
 					<label>Name</label>
-					<input type="text" id="name" name="name" {...formik.getFieldProps('name')} />
+					<input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} onBlur={formik.handleBlur} />
 					{formik.touched.name && formik.errors.name ? <div className="error">{formik.errors.name}</div> : null}
 				</div>
 				<div className="form-control">
 					<label>E-mail</label>
-					<input type="email" id="email" name="email" {...formik.getFieldProps('email')} />
+					<input type="email" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} onBlur={formik.handleBlur} />
 					{formik.touched.email && formik.errors.email ? <div className="error">{formik.errors.email}</div> : null}
 				</div>
 				<div className="form-control">
 					<label>Channel</label>
-					<input type="text" id="channel" name="channel" {...formik.getFieldProps('channel')} />
+					<input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} onBlur={formik.handleBlur} />
 					{formik.touched.channel && formik.errors.channel ? <div className="error">{formik.errors.channel}</div> : null}
 				</div>
 				<button type="submit">Submit</button>
@@ -52,5 +69,3 @@ const YouTubeForm = () => {
 export default YouTubeForm;
 
 // Formik'in handleBlur fonksiyonu, form elemanlarından biri, örneğin bir input alanı, odaklanmayı kaybettiğinde (blur event) çağrılan bir fonksiyondur. formik.touched ile kontrol edilir
-
-// getFieldProps Formik kütüphanesi tarafından sağlanan bir yardımcı fonksiyondur ve form elemanları için gerekli olan özellikleri içeren bir obje döndürür. Bu özellikler, form elemanının değerini, onChange ve onBlur olaylarını, validasyonu ve daha fazlasını içerebilir.
