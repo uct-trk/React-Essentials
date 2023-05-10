824068e67c1d9639d4d4c97ae3f9d8b534db7760
UCT <uct@ets-mac-0238.local>
2023-05-10 17:48:08 +0300

    Formik Component ,Formik, Form, Field, ErrorMessage

--- formik-advance/src/components/YouTubeForm.jsx
+++ formik-advance/src/components/YouTubeForm.jsx
@@ -1,4 +1,4 @@
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const initialValues = {
@@ -19,33 +19,27 @@ const validationSchema = Yup.object({
 });
 
 const YouTubeForm = () => {
-	const formik = useFormik({
-		initialValues: initialValues,
-		onSubmit: onSubmit,
-		// validate: validate
-		validationSchema: validationSchema,
-	});
 	return (
-		<div>
-			<form onSubmit={formik.handleSubmit}>
+		<Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+			<Form>
 				<div className="form-control">
 					<label>Name</label>
-					<input type="text" id="name" name="name" {...formik.getFieldProps('name')} />
-					{formik.touched.name && formik.errors.name ? <div className="error">{formik.errors.name}</div> : null}
+					<Field type="text" id="name" name="name" />
+					<ErrorMessage className="error" name="name" />
 				</div>
 				<div className="form-control">
 					<label>E-mail</label>
-					<input type="email" id="email" name="email" {...formik.getFieldProps('email')} />
-					{formik.touched.email && formik.errors.email ? <div className="error">{formik.errors.email}</div> : null}
+					<Field type="email" id="email" name="email" />
+					<ErrorMessage name="email" />
 				</div>
 				<div className="form-control">
 					<label>Channel</label>
-					<input type="text" id="channel" name="channel" {...formik.getFieldProps('channel')} />
-					{formik.touched.channel && formik.errors.channel ? <div className="error">{formik.errors.channel}</div> : null}
+					<Field type="text" id="channel" name="channel" />
+					<ErrorMessage name="channel" />
 				</div>
 				<button type="submit">Submit</button>
-			</form>
-		</div>
+			</Form>
+		</Formik>
 	);
 };
 
