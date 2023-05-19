f0f00647d1b2b7df2fe6b9762e92b71cbc4b6a90
UCT <uct@ets-mac-0238.local>
2023-05-19 17:09:23 +0300

    typescript primitive

---
+++ TypescriptCourse/src/components/DataAnnotation/annotation.ts
@@ -0,0 +1,6 @@
+let age:number = 28;
+let firstName: string = "Uğurcan"
+let lastName: string = "Türk"
+let isUpdated: boolean = true
+
+
---
+++ TypescriptCourse/src/components/Primitive/index.ts
@@ -0,0 +1,12 @@
+// Number
+let first:number = 28; // number
+let second: number = 0x47CF // hexadecimal
+let third: number = 0o377 // octal
+let fourth: number = 0b111001 // binary
+
+// String
+let firstName:string = "Uğurcan" // string
+let firstNameDesc:string = `My name is ${firstName}` // string
+
+// Boolean
+let isPresent:boolean = true // boolean
