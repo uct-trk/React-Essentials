90e3f8c0b3d02c5430ca119601f6d6c26d0ea503
UCT <uct@ets-mac-0238.local>
2023-05-19 17:37:00 +0300

    typescript object

---
+++ TypescriptCourse/src/components/Object/index.ts
@@ -0,0 +1,17 @@
+type Person = {
+    firstName:string,
+    lastName:string,
+    age:number,
+    title:string
+}
+
+let obj:Person;
+
+obj = {
+    firstName:"Uğur",
+    lastName:"Türk",
+    age: 27,
+    title: "Developer"
+}
+
+console.log(obj.age)
