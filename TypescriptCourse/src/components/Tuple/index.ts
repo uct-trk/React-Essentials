9d58a5021e56094472e7aef071b76213b3268f73
UCT <uct@ets-mac-0238.local>
2023-05-19 17:28:30 +0300

    typescript tuple

---
+++ TypescriptCourse/src/components/Tuple/index.ts
@@ -0,0 +1,11 @@
+let id:number = 100
+let instructorName:string = "Ugurcan"
+let instructor : [number,string] = [id,instructorName]
+
+let user:[number,string,boolean,number,string];
+    user= [1,"selam",true,20,"char"]
+
+let employee:[number,string][]
+    employee = [[1,"steve"], [2,"Bill"], [3,"Jeff"]]    
+
+// Tuple'larda, tanımlanan sıraya göre elemanlara erişilebilir ve her elemanın belirli bir türü olmalıdır. Tuple'lar, belirli bir yapıyı ifade etmek, belirli bir sayıda ve sıralamada değerleri taşımak veya önceden belirlenmiş bir veri formatını temsil etmek için kullanışlıdır.
