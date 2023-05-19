77659fbfe3200d0f312e46370382684cf5d693b7
UCT <uct@ets-mac-0238.local>
2023-05-19 17:19:29 +0300

    typescript arrays

---
+++ TypescriptCourse/src/components/Array/index.ts
@@ -0,0 +1,14 @@
+// Array
+let names: string[] = ["uğur","ümit","ahmet"] // string array
+let surnames: Array<string> = ["türk","selam","kıyı"] // string array
+
+let arr:Array<any> = [1,5,"selam",true] // her türden veri tipini içeren array
+
+let fruits: Array<string>
+fruits = ["Apple","Banana","Orange"] // string array
+
+let ids:Array<Number>;
+ids = [1,2,3] // number array
+
+let values:(string | number)[] = ["Apple", 2 , "Orange", "Banana", 100] // yalnızca string veya number veri tipi içerebilen array
+let keys:Array<string | number> = ["Apple", 2 , "Orange", "Banana", 100] // yalnızca string veya number veri tipi içerebilen array
