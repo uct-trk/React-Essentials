fef3b263356df5a6a024866f66a55a71c7aa39ef
UCT <uct@ets-mac-0238.local>
2023-05-20 17:18:42 +0300

    typescript switch case

---
+++ TypescriptCourse/src/components/SwitchCase/index.ts
@@ -0,0 +1,29 @@
+let day: number = 2;
+
+switch (day) {
+	case 0:
+		console.log('Pazar');
+		break;
+	case 1:
+		console.log('Pazartesi');
+		break;
+	case 2:
+		console.log('Salı');
+		break;
+	case 3:
+		console.log('Çarşamba');
+		break;
+	case 4:
+		console.log('Perşembe');
+		break;
+	case 5:
+		console.log('Cuma');
+		break;
+	case 6:
+		console.log('Cumartesi');
+		break;
+
+	default:
+		console.log('Hata');
+		break;
+}
