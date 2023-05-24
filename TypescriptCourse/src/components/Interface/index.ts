c8f97bb087c8c2529bb6d8b4ae9fb7ce2d794ec3
UCT <uct@ets-mac-0238.local>
2023-05-24 12:36:22 +0300

    typescript interfaces with optional param

--- TypescriptCourse/src/components/Interface/index.ts
+++ TypescriptCourse/src/components/Interface/index.ts
@@ -5,6 +5,7 @@ interface Kisi {
 	ad: string;
 	yas: number;
 	calisan: boolean;
+	gobekAdi?: string; // optional, zorunluluk içermez.
 }
 
 // Bir nesne tanımlama
@@ -12,12 +13,23 @@ const kisi: Kisi = {
 	ad: 'John',
 	yas: 30,
 	calisan: true,
+	gobekAdi: 'Doe',
 };
 
 // Bir fonksiyonun parametrelerini ve dönüş değerini belirtme
 function selamla(isim: string): void {
 	console.log('Merhaba, ' + isim);
 }
+
+function tamİsim(kisi: Kisi): void {
+	if (kisi.gobekAdi) {
+		// optioal olan gobek adı varsa if içerisine girer
+		console.log(`${kisi.ad} gobek adı: ${kisi.gobekAdi}`);
+	} else {
+		console.log(`${kisi.ad}`);
+	}
+}
+console.log(tamİsim(kisi));
 
 // Nesnenin özelliklerini kullanma
 console.log(kisi.ad); // "John"
