805143336162b2e4163f12cfdaa2e8331e8a4830
UCT <uct@ets-mac-0238.local>
2023-05-26 17:53:59 +0300

    mui grid , responsive

--- ReactMUI/src/components/GridMUI.tsx
+++ ReactMUI/src/components/GridMUI.tsx
@@ -18,16 +18,16 @@ export default function BasicGrid() {
 		<Stack spacing={4}>
 			<Box sx={{ flexGrow: 1 }}>
 				<Grid container spacing={2}>
-					<Grid item xs={8}>
+					<Grid item xs={12} md={8}>
 						<Item>xs=8</Item>
 					</Grid>
-					<Grid item xs={4}>
+					<Grid item xs={12} md={4}>
 						<Item>xs=4</Item>
 					</Grid>
-					<Grid item xs={4}>
+					<Grid item xs={12} md={4}>
 						<Item>xs=4</Item>
 					</Grid>
-					<Grid item xs={8}>
+					<Grid item xs={12} md={8}>
 						<Item>xs=8</Item>
 					</Grid>
 				</Grid>
