d6e7776e4238bec8fe8989e92f45691f05d9d096
UCT <uct@ets-mac-0238.local>
2023-05-26 14:24:37 +0300

    mui radio groups

--- ReactMUI/src/App.tsx
+++ ReactMUI/src/App.tsx
@@ -1,11 +1,12 @@
 import ButtonMUI from './components/ButtonMUI';
 import TypographyLesson from './components/TypographyLesson';
 import TextFieldMUI from './components/TextFieldMUI';
+import RadioGroupMUI from './components/RadioGroupMUI';
 
 function App() {
 	return (
 		<div>
-			<TextFieldMUI />
+			<RadioGroupMUI />
 		</div>
 	);
 }
---
+++ ReactMUI/src/components/RadioGroupMUI.tsx
@@ -0,0 +1,19 @@
+import { Radio, FormControl, FormLabel, FormControlLabel, RadioGroup, FormHelperText } from '@mui/material';
+import { useState } from 'react';
+
+const RadioGroupMUI = () => {
+	const [value, setValue] = useState('');
+	console.log(value);
+	return (
+		<FormControl>
+			<FormLabel color={`${value === '2000' ? 'primary' : value === '3000' ? 'secondary' : 'warning'}`}>Maaş Beklentiniz</FormLabel>
+			<RadioGroup row name="expected-salary" value={value} onChange={(e) => setValue(e.target.value)}>
+				<FormControlLabel label="2000$" value="2000" color="secondary" control={<Radio color="primary" />} />
+				<FormControlLabel label="3000$" value="3000" color="secondary" control={<Radio color="secondary" />} />
+				<FormControlLabel label="4000$" value="4000" color="secondary" control={<Radio color="warning" />} />
+			</RadioGroup>
+		</FormControl>
+	);
+};
+
+export default RadioGroupMUI;
