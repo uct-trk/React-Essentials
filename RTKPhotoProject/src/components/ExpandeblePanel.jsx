811f2778476dd41dcf254bdf0a686ac626cfafb1
UCT <uct@ets-mac-0238.local>
2023-06-22 12:28:00 +0300

    expanded panel

--- RTKPhotoProject/src/components/ExpandeblePanel.jsx
+++ RTKPhotoProject/src/components/ExpandeblePanel.jsx
@@ -1,17 +1,24 @@
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import { Stack } from '@mui/material';
+import { useState } from 'react';
 
 const ExpandeblePanel = ({ children, header }) => {
+	const [expanded, setExpanded] = useState(false);
+
+	const handleClick = () => {
+		setExpanded(!expanded);
+	};
+	console.log(expanded);
 	return (
 		<Stack padding={3} sx={{ backgroundColor: 'lightgrey', borderRadius: '30px' }}>
 			<Stack direction={'row'} justifyContent={'space-between'}>
 				<Stack>{header}</Stack>
-				<Stack>
-					<KeyboardArrowLeftIcon />
+				<Stack onClick={handleClick} sx={{ cursor: 'pointer' }}>
+					{expanded ? <KeyboardArrowDownIcon /> : <KeyboardArrowLeftIcon />}
 				</Stack>
 			</Stack>
-			{children}
+			{expanded && children}
 		</Stack>
 	);
 };
