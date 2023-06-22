06ec4a4b47c3c6ee57773466e1c8db99724003c6
UCT <uct@ets-mac-0238.local>
2023-06-22 14:03:59 +0300

    album list and remove album

--- RTKPhotoProject/src/components/AlbumItem.jsx
+++ RTKPhotoProject/src/components/AlbumItem.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import { Button, CircularProgress, Stack, Typography } from '@mui/material';
+import ExpandeblePanel from './ExpandeblePanel';
+import PhotoList from './PhotoList';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useRemoveAlbumMutation } from '../store';
 
 const AlbumItem = ({ album }) => {
-	return <div>{album.name}</div>;
+	const [deleteAlbum, result] = useRemoveAlbumMutation();
+
+	const handleDelete = async () => {
+		return await deleteAlbum(album).unwrap();
+	};
+
+	const header = (
+		<Stack direction={'row'} alignItems={'center'}>
+			<Button onClick={handleDelete} disabled={result.isLoading}>
+				{result.isLoading ? <CircularProgress style={{ width: '25px', height: '25px' }} /> : <DeleteIcon />}
+			</Button>
+			<Typography>{album.name}</Typography>
+		</Stack>
+	);
+	return (
+		<Stack>
+			<ExpandeblePanel header={header}>
+				<PhotoList album={album} />
+			</ExpandeblePanel>
+		</Stack>
+	);
 };
 
 export default AlbumItem;
